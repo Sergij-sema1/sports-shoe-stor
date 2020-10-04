fcc1ac1486c1335ba84e1daa8b2013c022c13ae6
sema1 <semkov.sergij@mail.ru>
2020-10-04 11:21:44 +0300

    fix html

--- js/main.js
+++ js/main.js
@@ -10,21 +10,47 @@ function toggleModal() {
 }
 
 
-//---------------------------------------
+//day1 1 //---------------------------------------
 
 const buttomAuth = document.querySelector('.button-auth');
 const modalAuth = document.querySelector('.modal-auth');
 const closeAuth = document.querySelector('.close-auth');
-//const logInForm = document.querySelector('#logInForm');
+const logInForm = document.querySelector('#logInForm');
+
+let login = '';
 
 modalAuth.classList.add('hello');
 
 
-buttomAuth.addEventListener('click', toggleModalAuth);
 
-closeAuth.addEventListener('click', toggleModalAuth);
 
 
 function toggleModalAuth() {
   modalAuth.classList.toggle('is-open');
+}
+
+function authorized() {
+  console.log('Aвторизован');
+}
+
+function notAuthorized() {
+
+
+  console.log('Не авторизован');
+
+  function login(event) {
+    console.log(event);
+    event.preventDefult();
+    console.log('логин');
+
+  }
+  buttomAuth.addEventListener('click', toggleModalAuth);
+  closeAuth.addEventListener('click', toggleModalAuth);
+  logInForm.addEventListener('submit', login);
+}
+
+if ('авторизован') {
+  authorized();
+} else if (неавторизован) {
+  notAuthorized();
 }
