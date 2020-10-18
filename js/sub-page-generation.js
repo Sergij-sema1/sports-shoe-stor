991f1d897903b61c482cd6f946d61c7f62e0f307
sema1 <semkov.sergij@mail.ru>
2020-10-18 20:34:50 +0300

    fix style

--- js/sub-page-generation.js
+++ js/sub-page-generation.js
@@ -1,28 +1,30 @@
-const idFromCookie = document.cookie;
+//функцыя получает данные из псевдобазы,генерирует карточки динамически на страницу по ид переданому coockies
+const subPageGEneration = () => {
+  const idFromCookie = document.cookie;
 
-//адрес базы данных откуда подгружаются все карточки
-const url = '/db/brends_catalog/allshose.json';
+  //адрес базы данных откуда подгружаются все карточки
+  const url = '/db/brends_catalog/allshose.json';
 
-//функцыя для обращения к базе
-const DataFormeBace = async (url, id) => {
+  //функцыя для обращения к базе
+  const DataFormeBace = async (url, id) => {
 
-  const response = await fetch(url);
-  return await response.json();
+    const response = await fetch(url);
+    return await response.json();
 
-}
-//функцыя которая получает дание и генерит из них карточки
-const cartGen = (list) => {
-  const {
-    id,
-    brandsId,
-    image,
-    name,
-    description,
-    description_second,
-    price
-  } = list;
+  }
+  //функцыя которая получает дание и генерит из них карточки
+  const cartGen = (list) => {
+    const {
+      id,
+      brandsId,
+      image,
+      name,
+      description,
+      description_second,
+      price
+    } = list;
 
-  const card = ` <a  href="brendscatalog.html " class="card " id="${id}" card-restaurant" >
+    const card = ` <a  href="brendscatalog.html " class="card " id="${id}" card-restaurant" >
               <img
                 src = "${image}"
                 alt="image"
@@ -40,19 +42,22 @@ const cartGen = (list) => {
               </div>
             </a>`;
 
-  const subCardRestaurant = document.querySelector('.cards-restaurants');
-  subCardRestaurant.insertAdjacentHTML('beforeend', card);
+    const subCardRestaurant = document.querySelector('.cards-restaurants');
+    subCardRestaurant.insertAdjacentHTML('beforeend', card);
 
 
 
-}
-//DataFormeBace возвращает обекти с id  который передан в cockis
-// с перебором в цыкле потом вызов функции для генерацыи карточек
-// с передачей ей даных
-DataFormeBace(url, idFromCookie).then((data) => {
+  }
+  //DataFormeBace возвращает обекти с id  который передан в cockis
+  // с перебором в цыкле потом вызов функции для генерацыи карточек
+  // с передачей ей даных
+  DataFormeBace(url, idFromCookie).then((data) => {
 
-  const list = data.filter(currentValue => {
-    return currentValue.brandsId == `${idFromCookie}`;
+    const list = data.filter(currentValue => {
+      return currentValue.brandsId == `${idFromCookie}`;
+    });
+    list.forEach(cartGen);
   });
-  list.forEach(cartGen);
-});
+};
+
+subPageGEneration();
