af778062769aba979a0852dcdfb813b819b3b856
sema1 <semkov.sergij@mail.ru>
2020-11-29 19:18:51 +0300

    ..

---
+++ js/finall.js
@@ -0,0 +1,31 @@
+const sliderImg = document.querySelector('.mainImg');
+const SliderList = document.querySelector('.listImg');
+const firstButtonSlider = document.querySelector('.btn1');
+const secondButtonSlider = document.querySelector('.btn2');
+
+//выбераем размер и он меняет цвет для визуальной фиксацsb
+const ItemSizeSelected = (event) => {
+
+    const target = event.target;
+
+    if (target.nodeName == "LI") {
+        target.classList.toggle('selected');
+        console.log(target.id)
+
+    } else {
+        return;
+    }
+
+
+};
+//функция после вызова обрабатывает нажатие нажатых клавиш слайдера
+const clickButton = (item) => {
+    console.log(item.srcElement)
+
+};
+
+
+
+firstButtonSlider.addEventListener('click', clickButton);
+secondButtonSlider.addEventListener('click', clickButton);
+SliderList.addEventListener('click', ItemSizeSelected);
