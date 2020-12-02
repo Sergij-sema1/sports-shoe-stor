24783ee2e99d4f13ada323484c979436affcabb6
sema1 <semkov.sergij@mail.ru>
2020-12-02 19:33:38 +0300

    .

--- js/finall.js
+++ js/finall.js
@@ -1,12 +1,13 @@
 const dataFromCookie = document.cookie;
-const DbUrl = 'http://localhost:8080/shop/brand';
-const sliderImg = document.querySelector('.mainImg');
-const SliderList = document.querySelector('.listImg');
-const firstButtonSlider = document.querySelector('.btn1');
-const secondButtonSlider = document.querySelector('.btn2');
+const DbUrl = "http://localhost:8080/shop/model";
+const sliderImg = document.querySelector(".mainImg");
+const SliderList = document.querySelector(".listImg");
+const firstButtonSlider = document.querySelector(".btn1");
+const secondButtonSlider = document.querySelector(".btn2");
+const mainSlider = document.querySelector(".MainSlider");
+const description = document.querySelector('.description');
 
-
-//функцыя делает запрос к базе для получения данных 
+//функцыя делает запрос к базе для получения данных
 const GetdadaItem = async (url) => {
     const response = await fetch(url);
     if (!response.ok) {
@@ -16,39 +17,77 @@ const GetdadaItem = async (url) => {
     return response.json();
 };
 
-
-
-
-
 //выбераем размер и он меняет цвет для визуальной фиксацыи
 const ItemSizeSelected = (event) => {
-
     const target = event.target;
 
     if (target.nodeName == "LI") {
-        target.classList.toggle('selected');
-        console.log(target.id)
-
+        target.classList.toggle("selected");
+        console.log(target.id);
     } else {
         return;
     }
+};
+//функцыя создает структуру страницы (картинки и размеры с данных полученных от бд)
+const createItem = (ProductList) => {
+    const {
+        id,
+        brandid,
+        img,
+        name,
+        description,
+        description_second,
+        price,
+    } = ProductList;
 
+
+    const cardItem = `<img
+     src="/img/brands_shouse_foto/${img}"
+     class="mainImg"
+     alt=""
+        />
+
+        <div class="box">
+            <ul class="listImg">
+            <li id="1">37</li>
+            <li id="2">38</li>
+            <li id="3">39</li>
+            <li id="4">40</li>
+            <li id="5">41</li>
+            <li id="6">42</li>
+            <li id="7">43</li>
+            <li id="8">44</li>
+            <li id="9">45</li>
+            <li id="10">46</li>
+            <li id="11">47</li>
+          </ul>
+        </div>
+
+        
+
+      
+        `;
+
+
+
+
+    mainSlider.insertAdjacentHTML('beforeend', cardItem);
 
 };
+
 //функция после вызова обрабатывает нажатие нажатых клавиш слайдера
 const clickButton = (item) => {
-    console.log(item.srcElement)
-
+    console.log(item.srcElement);
 };
 
+GetdadaItem(DbUrl, dataFromCookie).then((data) => {
+    const item = data;
+    const valueData = item.filter((i) => {
+        return i.id == `${dataFromCookie}`;
+    });
+    valueData.forEach(createItem);
+});
 
-GetdadaItem(DbUrl).then(
-    (data) => {
-        console.log(data);
-    }
-)
-
-
-firstButtonSlider.addEventListener('click', clickButton);
-secondButtonSlider.addEventListener('click', clickButton);
-SliderList.addEventListener('click', ItemSizeSelected);
+firstButtonSlider.addEventListener("click", clickButton);
+secondButtonSlider.addEventListener("click", clickButton);
+//SliderList.addEventListener('click', ItemSizeSelected);
