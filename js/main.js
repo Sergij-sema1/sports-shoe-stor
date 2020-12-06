931133225f1b91191910db71e07dec63dfb708d5
sema1 <semkov.sergij@mail.ru>
2020-12-06 22:58:42 +0300

    add.autorized

--- js/main.js
+++ js/main.js
@@ -1,36 +1,96 @@
 const cartButton = document.querySelector("#cart-button");
 const modal = document.querySelector(".modal");
 const close = document.querySelector(".close");
-const buttomAuth = document.querySelector('.button-auth');
+const buttonAuth = document.querySelector('.button-auth');
 const modalAuth = document.querySelector('.modal-auth');
 const closeAuth = document.querySelector('.close-auth');
 const logInForm = document.querySelector('#logInForm');
 const loginInput = document.querySelector('#login');
-const userName = document.querySelector('.user-name');
-const LoginForm = document.querySelector('.form-Main')
 
-let login = '';
+const buttonLogin = document.querySelector('.button-login');
+const passFromInput = document.querySelector('#password');
+const userName = document.querySelector('.user-name');
+const buttonOut = document.querySelector('.button-out');
+
+
+// let login = loginInput.value;
+// let pass = passFromInput.value;
+let login = localStorage.getItem('userLogin');
+let pass = localStorage.getItem('userPass');
 
 const toggleModal = () => {
-  console.log('экнопка корзина нажата');
   modal.classList.toggle("is-open");
-
 }
 
 const toggleModalAuth = () => {
-  console.log('кнопка воити нажата');
-
   modalAuth.classList.toggle('is-open');
-
-
-
-
-
 }
 
-buttomAuth.addEventListener('click', toggleModalAuth);
+
+
+const authorized = () => {
+  userName.textContent = login;
+  buttonAuth.style.display = "none";
+  userName.style.display = "inline";
+  buttonOut.style.display = "block";
+
+  const logOut = () => {
+    login = null;
+    localStorage.removeItem('userLogin');
+    localStorage.removeItem('userPass');
+    buttonAuth.style.display = "";
+    userName.style.display = "";
+    buttonOut.style.display = "";
+    buttonOut.removeEventListener('click', logOut);
+    checkAuth();
+
+
+  };
+
+  buttonOut.addEventListener('click', logOut);
+
+
+
+
+  console.log('авторизоан')
+};
+
+
+const notAuthorized = (event) => {
+  const logIn = (event) => {
+    event.preventDefault();
+    login = loginInput.value;
+    pass = passFromInput.value;
+    localStorage.setItem('userLogin', login);
+    localStorage.setItem('userPass', pass);
+    toggleModalAuth();
+    buttonAuth.removeEventListener('click', toggleModalAuth);
+    closeAuth.removeEventListener("click", toggleModalAuth);
+    logInForm.removeEventListener('submit', logIn);
+    logInForm.reset();
+    checkAuth();
+
+
+  };
+
+  buttonAuth.addEventListener('click', toggleModalAuth);
+  closeAuth.addEventListener("click", toggleModalAuth);
+  logInForm.addEventListener('submit', logIn);
+  console.log(' логин не введен')
+
+};
+
+
 cartButton.addEventListener("click", toggleModal);
-closeAuth.addEventListener("click", toggleModalAuth);
 close.addEventListener('click', toggleModal);
 
-//logInForm.addEventListener('submit', logIn);
+
+const checkAuth = () => {
+  if (login) {
+
+    authorized();
+  } else {
+    notAuthorized();
+  }
+}
+checkAuth();
