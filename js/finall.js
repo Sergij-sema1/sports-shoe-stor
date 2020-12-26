65723b58021049013f27c9760e3cf4d7ca4547b3
sema1 <semkov.sergij@mail.ru>
2020-12-26 12:42:47 +0300

    .

--- js/finall.js
+++ js/finall.js
@@ -18,37 +18,38 @@ class="brandInput" id="inputSring">`;
 
 // после нажатия оформить будет брать все данных записывать их в базу и через бота в телеграмме будет деать отчет продавцу)
 const Checkout = () => {
-        const buyerContact = document.querySelector('#inputSring').value;
-        const itemName = document.querySelector('.food-name').textContent;
-        const itemCount = document.querySelector('.counter').textContent;
-        const itemPrice = document.querySelector('.food-price').textContent;
-        const itemTotalPrice = document.querySelector('.modal-pricetag').textContent;
-        const timeBy = new Date();
-        const date = timeBy.toLocaleDateString();
+    const buyerContact = document.querySelector('#inputSring').value;
+    const itemName = document.querySelector('.food-name').textContent;
+    const itemCount = document.querySelector('.counter').textContent;
+    const itemPrice = document.querySelector('.food-price').textContent;
+    const itemTotalPrice = document.querySelector('.modal-pricetag').textContent;
+    const timeBy = new Date();
+    const date = timeBy.toLocaleDateString();
 
-        const deliveryData = `${buyerContact},${itemName},кол:${itemCount},цена:${itemPrice},об.цена:${itemTotalPrice},${date}`;
+    const deliveryData = `
+${buyerContact},${itemName},кол:${itemCount},цена:${itemPrice},об.цена:${itemTotalPrice},${date}`;
 
 
 
 
-        const addOrderToDb = async () => {
+    const addOrderToDb = async () => {
 
-                    const urlAddOrderToDb = 'http://localhost:8080/shop/order/by';
-                    let response = await fetch(urlAddOrderToDb, {
-                        method: 'POST',
-                        headers: {
+        const urlAddOrderToDb = 'http://localhost:8080/shop/order/by';
+        let response = await fetch(urlAddOrderToDb, {
+            method: 'POST',
+            headers: {
 
-                            'Content-Type': ' application/json;charset=utf-8'
-                        },
-                        body: JSON.stringify({
-                            buyerData: `${deliveryData}`
-                        })
-                    });
-                    console.log(response.status)
-                    if (response.ok) {
-                        return console.log(`add item to db ok `);
-                    } else {
-                        return console.log(`add item to db err );
+                'Content-Type': ' application/json;charset=utf-8'
+            },
+            body: JSON.stringify({
+                buyerData: `${deliveryData}`
+            })
+        });
+        console.log(response.status)
+        if (response.ok) {
+            return console.log(`add item to db ok `);
+        } else {
+            return console.log(`add item to db e`);
         }
 
     }
@@ -72,14 +73,8 @@ let sizeData = [];
 const GetdadaItem = async (url) => {
     const response = await fetch(url);
     if (!response.ok) {
-        throw new Error(`
-                            ошибка по адресу: $ {
-                                url
-                            },
-                            код ошибки: $ {
-                                response.status
-                            }
-                            `);
+        throw new Error(`ошибка по адресу :${url},
+    код ошибки :${response.status}`);
     }
     return response.json();
 };
@@ -112,139 +107,133 @@ const createItem = (ProductList) => {
         price,
     } = ProductList;
 
-    const cardItem = ` < img src = "/img/brands_shouse_foto/${img}"
-                            class = "mainImg"
-                            alt = "" /
-                            >
+    const cardItem = `<img
+     src="/img/brands_shouse_foto/${img}"
+     class="mainImg"
+     alt=""
+        />
 
-                            <
-                            div class = "box" >
-                            <
-                            ul class = "listImg sizBox" >
-                            <
-                            li id = "1" > 37 < /li> <
-                            li id = "2" > 38 < /li> <
-                            li id = "3" > 39 < /li> <
-                            li id = "4" > 40 < /li> <
-                            li id = "5" > 41 < /li> <
-                            li id = "6" > 42 < /li> <
-                            li id = "7" > 43 < /li> <
-                            li id = "8" > 44 < /li> <
-                            li id = "9" > 45 < /li> <
-                            li id = "10" > 46 < /li> <
-                            li id = "11" > 47 < /li> <
-                            /ul> <
-                            /div>
-                            `;
+        <div class="box">
+            <ul class="listImg sizBox">
+            <li id="1">37</li>
+            <li id="2">38</li>
+            <li id="3">39</li>
+            <li id="4">40</li>
+            <li id="5">41</li>
+            <li id="6">42</li>
+            <li id="7">43</li>
+            <li id="8">44</li>
+            <li id="9">45</li>
+            <li id="10">46</li>
+            <li id="11">47</li>
+          </ul>
+        </div>
+`;
 
     mainSlider.insertAdjacentHTML("beforeend", cardItem);
-    const cartDescription = ` < p > $ {
-                                description
-                            }, $ {
-                                price
-                            } < /p>`;
-                            dataItem.insertAdjacentHTML("beforeend", cartDescription);
-                        };
+    const cartDescription = ` <p>${description},${price}</p>`;
+    dataItem.insertAdjacentHTML("beforeend", cartDescription);
+};
 
-                        //функция после вызова обрабатывает нажатие нажатых клавиш слайдера
-                        const clickButton = (item) => {
-                            console.log(item.srcElement);
-                        };
+//функция после вызова обрабатывает нажатие нажатых клавиш слайдера
+const clickButton = (item) => {
+    console.log(item.srcElement);
+};
 
-                        GetdadaItem(DbUrl, dataFromCookie).then((data) => {
-                            const item = data;
-                            const valueData = item.filter((i) => {
-                                return i.id == `${dataFromCookie}`;
-                            });
-                            valueData.forEach(createItem);
-                        });
+GetdadaItem(DbUrl, dataFromCookie).then((data) => {
+    const item = data;
+    const valueData = item.filter((i) => {
+        return i.id == `${dataFromCookie}`;
+    });
+    valueData.forEach(createItem);
+});
 
-                        // функцыя добавляет товар в кoрзину путьом создания елемента внедрение его на страницу и потом пушит в масив
-                        const addToCart = (event) => {
-                            const itemDataFromLocal = JSON.parse(localStorage.getItem("item"));
-                            const modalx = document.querySelector('.modal-x');
-                            modalx.style.display = 'none';
+// функцыя добавляет товар в кoрзину путьом создания елемента внедрение его на страницу и потом пушит в масив
+const addToCart = (event) => {
+    const itemDataFromLocal = JSON.parse(localStorage.getItem("item"));
+    const modalx = document.querySelector('.modal-x');
+    modalx.style.display = 'none';
 
 
-                            const {
-                                id,
-                                brandid,
-                                img,
-                                name,
-                                description,
-                                description_second,
-                                price,
-                            } = itemDataFromLocal;
+    const {
+        id,
+        brandid,
+        img,
+        name,
+        description,
+        description_second,
+        price,
+    } = itemDataFromLocal;
 
-                            const cardBody = document.querySelector(".modal-header");
+    const cardBody = document.querySelector(".modal-header");
 
 
-                            if (buttonAddToCart) {
-                                //пока незаработало чтото перемудрил
+    if (buttonAddToCart) {
+        //пока незаработало чтото перемудрил
 
-                                const addCountIncard = () => {
-                                    const buttonPlus = document.querySelector('.buttonP');
-                                    const buttonMinus = document.querySelector('.buttonM');
-                                    const total = price;
-                                    const filterCard = cartDataArray.find((productCheck) => {
-                                        return productCheck.itemId === itemId;
-                                    });
-                                    modalPriceTag.textContent = `${price}₽`;
-                                    //перезаписывает коечную цену в зависимости от количества
-                                    const totalPrice = () => {
+        const addCountIncard = () => {
+            const buttonPlus = document.querySelector('.buttonP');
+            const buttonMinus = document.querySelector('.buttonM');
+            const total = price;
+            const filterCard = cartDataArray.find((productCheck) => {
+                return productCheck.itemId === itemId;
+            });
+            modalPriceTag.textContent = `${price}₽`;
+            //перезаписывает коечную цену в зависимости от количества
+            const totalPrice = () => {
 
-                                        modalPriceTag.textContent = `${parseFloat(total)*filterCard.itemCount} ₽`;
+                modalPriceTag.textContent = `${parseFloat(total)*filterCard.itemCount} ₽`;
 
-                                    };
+            };
 
-                                    const countAdd = () => {
+            const countAdd = () => {
 
 
 
-                                        if (filterCard.itemCount === 0) {
-                                            buttonMinus.style.display = 'flex';
-                                        }
-                                        filterCard.itemCount++;
-                                        const itemCountInDom = document.querySelector('.counter');
-                                        itemCountInDom.textContent = `${filterCard.itemCount}`;
-                                        modalPriceTag.textContent = `${total}`;
-                                        if (filterCard.itemCount > 1) {
-                                            totalPrice();
+                if (filterCard.itemCount === 0) {
+                    buttonMinus.style.display = 'flex';
+                }
+                filterCard.itemCount++;
+                const itemCountInDom = document.querySelector('.counter');
+                itemCountInDom.textContent = `${filterCard.itemCount}`;
+                modalPriceTag.textContent = `${total}`;
+                if (filterCard.itemCount > 1) {
+                    totalPrice();
 
 
-                                        }
-                                    };
-                                    const countDell = () => {
-                                        filterCard.itemCount--;
-                                        const itemCountInDom = document.querySelector('.counter');
-                                        itemCountInDom.textContent = `${filterCard.itemCount}`;
-                                        totalPrice();
-
-
-
-                                        if (filterCard.itemCount === 0) {
-                                            cartDataArray.splice(cartDataArray.indexOf(filterCard), 1);
-                                            buttonMinus.style.display = 'none';
+                }
+            };
+            const countDell = () => {
+                filterCard.itemCount--;
+                const itemCountInDom = document.querySelector('.counter');
+                itemCountInDom.textContent = `${filterCard.itemCount}`;
+                totalPrice();
 
 
 
-                                        }
+                if (filterCard.itemCount === 0) {
+                    cartDataArray.splice(cartDataArray.indexOf(filterCard), 1);
+                    buttonMinus.style.display = 'none';
 
-                                    };
+
+
+                }
+
+            };
 
 
 
-                                    buttonPlus.addEventListener('click', countAdd);
-                                    buttonMinus.addEventListener('click', countDell);
+            buttonPlus.addEventListener('click', countAdd);
+            buttonMinus.addEventListener('click', countDell);
 
 
 
 
 
-                                };
-                                const checkCard = document.querySelector('.item-in-cart');
-                                if (!checkCard) {
-                                    const card = `
+        };
+        const checkCard = document.querySelector('.item-in-cart');
+        if (!checkCard) {
+            const card = `
             <div class = "modal-body item-in-cart" id = "${id}">
                 <div class="food-row">
                     <span class="food-name">${name}</span>
@@ -256,65 +245,65 @@ const createItem = (ProductList) => {
                 </div> 
             </div>`;
 
-                                    cardBody.insertAdjacentHTML('afterend', card);
+            cardBody.insertAdjacentHTML('afterend', card);
 
 
 
-                                }
-                                buttonAddToCart.style.color = 'red';
-                                const itemId = document.querySelector('.item-in-cart').id;
-                                const itemCount = document.querySelector('.counter').textContent;
-                                const itemName = document.querySelector('.food-name').textContent;
-                                const itemPrice = document.querySelector('.food-price').textContent;
-                                const itemSize = JSON.stringify(Number.parseFloat(sizeData)); //угар)
+        }
+        buttonAddToCart.style.color = 'red';
+        const itemId = document.querySelector('.item-in-cart').id;
+        const itemCount = document.querySelector('.counter').textContent;
+        const itemName = document.querySelector('.food-name').textContent;
+        const itemPrice = document.querySelector('.food-price').textContent;
+        const itemSize = JSON.stringify(Number.parseFloat(sizeData)); //угар)
 
 
 
-                                buttonAddToCart.textContent = ' товар в корзине';
-                                // buttonAddToCart.removeEventListener('click', addToCart);
+        buttonAddToCart.textContent = ' товар в корзине';
+        // buttonAddToCart.removeEventListener('click', addToCart);
 
 
 
-                                //localStorage.setItem(' cartData', JSON.stringify(cartDataArray));
+        //localStorage.setItem(' cartData', JSON.stringify(cartDataArray));
 
-                                const product = cartDataArray.find((productCheck) => {
-                                    return productCheck.itemId === itemId;
-                                });
+        const product = cartDataArray.find((productCheck) => {
+            return productCheck.itemId === itemId;
+        });
 
-                                if (product) {
-                                    addCountIncard();
-
-
-                                } else if (!product) {
-                                    cartDataArray.push({
-                                        itemId,
-                                        itemName,
-                                        itemPrice,
-                                        itemSize,
-                                        itemCount: 1
-                                    });
-
-                                    modalPriceTag.insertAdjacentHTML('afterend', input);
-                                    addCountIncard();
-
-                                }
-                            };
+        if (product) {
+            addCountIncard();
 
 
-                        }
+        } else if (!product) {
+            cartDataArray.push({
+                itemId,
+                itemName,
+                itemPrice,
+                itemSize,
+                itemCount: 1
+            });
+
+            modalPriceTag.insertAdjacentHTML('afterend', input);
+            addCountIncard();
+
+        }
+    };
 
 
-                        clearCart.addEventListener('click', (event) => {
-                            const body = document.querySelector('.food-row');
-                            body.textContent = "";
-                            cartDataArray = [];
+}
 
-                            location.reload();
 
-                        })
+clearCart.addEventListener('click', (event) => {
+    const body = document.querySelector('.food-row');
+    body.textContent = "";
+    cartDataArray = [];
 
-                        buttonPrimary.addEventListener("click", Checkout);
-                        buttonAddToCart.addEventListener("click", addToCart);
-                        firstButtonSlider.addEventListener("click", clickButton);
-                        secondButtonSlider.addEventListener("click", clickButton);
-                        document.addEventListener("click", SizeSelected);
+    location.reload();
+
+})
+
+buttonPrimary.addEventListener("click", Checkout);
+buttonAddToCart.addEventListener("click", addToCart);
+firstButtonSlider.addEventListener("click", clickButton);
+secondButtonSlider.addEventListener("click", clickButton);
+document.addEventListener("click", SizeSelected);
